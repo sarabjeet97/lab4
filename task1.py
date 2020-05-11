9d31a43ead8c80f082bd914a35cb6a9da2e97c04
sarabjeet <sarabjeetsingh@dal.ca>
2020-05-11 16:43:28 -0300

    task1.py completed

--- task1.py
+++ task1.py
@@ -1,5 +1,17 @@
 #first program
-fn = open("1342-0.txt")
-contents = fn.read()
-print(contents)
+
+fn1 = open("1342-0.txt")
+content = fn1.read()
+list2 = content.strip(" ")
+list3 = " "
+list4 = " "
+for words in list2:
+  
+  if words == " " or words == " ,":
+    list3= list2.replace(" ","")
+    list4 = list3.strip("#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+  else:
+    pass
+
+print(list4.lower())
 
